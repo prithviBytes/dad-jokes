733c85c16a3b706054aa6928026366a3c47fcfd0
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2021-01-13 16:01:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import JokeList from "./JokeList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
